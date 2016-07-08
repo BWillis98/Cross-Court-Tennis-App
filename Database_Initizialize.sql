08b8eb096610ee402dbe09aaf34ee4037f994826
Brandon Willis <bwmusiclover1212@gmail.com>
2016-07-07 21:07:55 -0500

    Update Database

--- Database_Initizialize.sql
+++ Database_Initizialize.sql
@@ -1,7 +1,5 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -21,13 +19,13 @@ USE `Cross_Court_Tennis`;
 
 CREATE TABLE IF NOT EXISTS `Players` (
   `id` int(15) NOT NULL AUTO_INCREMENT,
-  `bio` varchar(350),
-  `favorite_shot` varchar(30),
-  `points` int(15) NOT NULL,
+  `bio` varchar(350), --Personal Bio
+  `favorite_shot` varchar(30), --Personal Choice
+  `points` int(15) NOT NULL, --For ranking
   `wins` int(5) NOT NULL,
   `losses` int(5) NOT NULL,
   `declines` int(1) DEFAULT 2, --How many matches one can decline before something happens
-  `declined_reports` DEFAULT 2, --How many score reports someone can get declined before something happens
+  `declined_reports` int(1) DEFAULT 2, --How many score reports someone can get declined before something happens
   `zip_code` int(11) NOT NULL, --Will use this to find courts nearby.
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
@@ -38,12 +36,20 @@ CREATE TABLE IF NOT EXISTS `Players` (
 -- Table structure for table `Matches`
 --
 
-CREATE TABLE IF NOT EXISTS `matches` (
-  `player_one` int(11) NOT NULL AUTO_INCREMENT,
-  `player_two` varchar(60) NOT NULL,
-  `product_img_name` varchar(60) NOT NULL,
-  `price` decimal(10,2) NOT NULL,
-  `typeOf` varchar(60) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Matches` (
+  `id` int(15) NOT NULL AUTO_INCREMENT,
+  `player_one` int(11) NOT NULL AUTO_INCREMENT, --Will be by ID
+  `player_two` varchar(60) NOT NULL, --Will be by ID
+  
+  --Set Scores
+  `Set1_player_one` int(1) NOT NULL,
+  `Set1_player_two` int(1) NOT NULL,
+  `Set2_player_one` int(1) NOT NULL,
+  `Set2_player_two` int(1) NOT NULL,
+  --End Set Scores
+  
+  `Set3_player_one` int(1) NOT NULL DEFAULT 0, --Defaulted to 0 because a third set is not always played
+  `Set3_player_two` int(1) NOT NULL DEFAULT 0, --Defaulted to 0 because a third set is not always played
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
